8e7bfae0da06947a40f39e1a5ed2b7cda9bc0861
WuDu <544587448@qq.com>
2026-03-17 21:43:31 +0800

    docs：Python匿名函数复习

---
+++ First_month/Day006/day06_python_lambda.py
@@ -0,0 +1,46 @@
+"""匿名函数"""
+#匿名函数，也叫 lambda 函数，是一种创建函数的语法，可以理解为匿名函数。
+#匿名函数的语法格式为：lambda [参数列表]: [表达式]
+square1 = lambda x: x*x
+def square2(x):
+    return x*x
+hello1 = lambda : print("hello world")
+def hello2():
+    print("hello world")
+#参数列表为空时，参数列表可以省略
+
+#运用案例
+#完成下列排序，按照每一个元素的字符串长度进行排序
+a = ["hello","world","python","java"]
+a.sort(key=lambda x:len(x))#适合作为一次性回调函数
+print(a)
+
+
+"""递归"""
+#递归，是指函数调用自身，递归函数必须包含一个递归终止条件，否则会进入无限循环。
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+""""电商订单计算器"""
+#定义一个函数，用于根据传入的一批商品信息（商品名称，商品价格，商品数量），优惠（满减、打折），运费信息计算订单的总价。
+#优惠规则如下：优惠券需要商品金额慢5000，且优惠券金额不能超过商品总价
+#积分抵扣 需要商品金额慢5000，100积分抵扣一元，积分只能整百抵扣
+
+def order_calculator(*args,coupon_price,integral):
+
+#计算商品总价
+    total_price = sum([x[1]*x[2] for x in args])
+#优惠券
+    if total_price > 5000 and coupon_price <= total_price:
+        total_price -= coupon_price
+#积分抵扣
+    if total_price > 5000 and integral % 100 == 0:
+        total_price -= integral // 100
+#运费
+    if total_price < 100:
+        total_price += 10
+#返回订单总价
+    return total_price
